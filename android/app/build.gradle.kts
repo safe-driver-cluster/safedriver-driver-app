645eaaa9f69174ccb3056ba4714fa1e5ab1c9ce2
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-05-08 22:28:42 +0530

    Update build.gradle.kts to correct application ID and add FlutterFire configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.safedriver.driver.safe_driver_driver_app"
+    namespace = "com.example.safe_driver_driver_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -23,7 +27,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.safedriver.driver.safe_driver_driver_app"
+        applicationId = "com.example.safe_driver_driver_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
