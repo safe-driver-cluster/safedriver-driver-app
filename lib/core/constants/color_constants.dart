3b090742e85be7c39b44b7ad58de08a6562b3b79
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-06-03 21:35:05 +0530

    feat: update color constants for improved theming and add hero gradient

--- lib/core/constants/color_constants.dart
+++ lib/core/constants/color_constants.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primaryColor = Color(0xFF2563EB);
-  static const primaryDark = Color(0xFF1D4ED8);
-  static const primaryLight = Color(0xFF60A5FA);
+  static const primaryColor = Color(0xFF2F6BFF);
+  static const primaryDark = Color(0xFF1746C7);
+  static const primaryLight = Color(0xFF6D93FF);
   static const secondaryColor = Color(0xFF10B981);
   static const warningColor = Color(0xFFF59E0B);
-  static const dangerColor = Color(0xFFEF4444);
+  static const dangerColor = Color(0xFFEF233C);
   static const infoColor = Color(0xFF06B6D4);
-  static const surfaceLight = Color(0xFFF8FAFC);
+  static const purpleColor = Color(0xFF8B3FF4);
+  static const surfaceLight = Color(0xFFF3F6FF);
+  static const cardTint = Color(0xFFEAF0FF);
   static const surfaceDark = Color(0xFF111827);
   static const textPrimary = Color(0xFF111827);
   static const textSecondary = Color(0xFF6B7280);
+
+  static const heroGradient = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [Color(0xFF1D4ED8), Color(0xFF2F6BFF), Color(0xFF8EA8FF)],
+  );
 }
